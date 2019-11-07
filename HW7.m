0d5c8090556924339ab27fba3b54b473c379e54e
warmflasha <warmflasha@gmail.com>
2019-11-07 14:07:51 -0600

    Wrote hw7

---
+++ HW7.m
@@ -0,0 +1,29 @@
+%HW7
+
+% Problem 1. Curve fitting. 
+% Part 1. Take 10 values for x on the interval 0 < x <= 10 and then create y
+% values from the x values by plugging the x values into a third order
+% polynomial of your choice. Add random noise to the data by choosing a random number
+% in the interval [-D, D]. Start with D = 1. Plot your data in the x-y plane.
+% Part 2. Fit your data with polynomials from order 1 to 9. Plot the fitted
+% polynomials together with the data. 
+% Part 3. On a separate plot, plot the R^2 and adjusted R^2 as a function
+% of the order of the polynomial. 
+% Part 4. Repeat parts 1 - 3 for D = 10 and D = 1000. Comment on the
+% results. 
+% Part 5. Now repeat parts 1-3 but take values for x on the interval 0 < x <=
+% 10. Comment on the results. 
+
+% Problem 2. Basic statistics. 
+% Part 1. Consider two different distributions - Gaussian numbers with a mean of
+% 0 and variance 1 and Gaussian numbers with a mean of 1 and variance 1.
+% (1) Make a plot of the average p-value for the t-test comparing N random
+% numbers chosen from each of these two distributions as a function of N.
+% Part 2. Now keep the first distribution the same, but vary the mean of
+% the second distribution between 0 and 10 with the same variance and
+% repeat part one. Make a plot of all of these different curves on the same
+% set of axes. What is different about the case where the mean of the second
+% distribution is 0? 
+% Part 3. Now keep the means of the two distributions at 0 and 1 as in part
+% 1, but vary the variance of both distributions simultaneiously between 0.1 and 10 and plot the 
+% p-values vs the number of numbers drawn as before. Comment on your results.  
